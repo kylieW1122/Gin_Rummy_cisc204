c4e027a8452a753a907a4eb519f9d45ded3d02c5
kylieW1122 <monkeykylie1122@gmail.com>
2023-10-25 16:20:26 -0400

    Built deck and required propositions

--- run.py
+++ run.py
@@ -1,23 +1,82 @@
 
 from bauhaus import Encoding, proposition, constraint
 from bauhaus.utils import count_solutions, likelihood
-
+from itertools import product
+import random
 # These two lines make sure a faster SAT solver is used.
 from nnf import config
 config.sat_backend = "kissat"
 
+#----------------Constants-----------------
+RANKS = [1,2,3,4,5,6,7,8,9]
+SUITS = ['A', 'B', 'C', 'D']
+NUM_OF_CARDS = 10
+#-------------Global Variables-------------
+deck = []
+discard = []
+player_cards = []
+
 # Encoding that will store all of your constraints
 E = Encoding()
 
-# To create propositions, create classes for them first, annotated with "@proposition" and the Encoding
-@proposition(E)
-class BasicPropositions:
+class Hashable: #recommanded
+    def __hash__(self):
+        return hash(str(self))
 
-    def __init__(self, data):
-        self.data = data
+    def __eq__(self, __value: object) -> bool:
+        return hash(self) == hash(__value)
 
     def __repr__(self):
-        return f"A.{self.data}"
+        return str(self)
+ # To create propositions, create classes for them first, annotated with "@proposition" and the Encoding   
+@proposition(E)
+class Player(Hashable):
+    def __init__(self, a, b): # a = rank, b = suit
+        self.a = a
+        self.b = b
+
+    def __str__(self):
+        return f"P({self.a}{self.b})"
+@proposition(E)
+class Opponent(Hashable):
+    def __init__(self, a, b): # a = rank, b = suit
+        self.a = a
+        self.b = b
+
+    def __str__(self):
+        return f"O({self.a}{self.b})"
+@proposition(E)
+class Run(Hashable):
+    def __init__(self, related_cards):
+        self.related_run_cards = related_cards
+
+    def __str__(self):
+        return f"Player has a meld of RUN that includes[{self.related_run_cards}]"
+
+@proposition(E)
+class Set(Hashable):
+    def __init__(self, related_cards):
+        self.related_set_cards = related_cards
+
+    def __str__(self):
+        return f"Player has a meld of SET that includes[{self.related_set_cards}]"
+
+
+def initial_game():
+    # reset the two global variable and create a shuffled deck
+    deck = []
+    discard = []
+    deck = list (product (RANKS, SUITS))
+    random.shuffle(deck)
+    # print(deck)
+    # distribute cards to the player and opponent
+    player_cards = deck[:NUM_OF_CARDS]
+    # opponent = Player('Opponent', deck[NUM_OF_CARDS:NUM_OF_CARDS*2])
+    # deck = deck[NUM_OF_CARDS*2:]
+    # print("deck:", deck)
+    # print(player_cards)
+    # print(opponent)
+    return player_cards
 
 
 # Different classes for propositions are useful because this allows for more dynamic constraint creation
@@ -25,26 +84,6 @@ class BasicPropositions:
 # that are instances of this class must be true by using a @constraint decorator.
 # other options include: at most one, exactly one, at most k, and implies all.
 # For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
-@constraint.at_least_one(E)
-@proposition(E)
-class FancyPropositions:
-
-    def __init__(self, data):
-        self.data = data
-
-    def __repr__(self):
-        return f"A.{self.data}"
-
-# Call your variables whatever you want
-a = BasicPropositions("a")
-b = BasicPropositions("b")   
-c = BasicPropositions("c")
-d = BasicPropositions("d")
-e = BasicPropositions("e")
-# At least one of these will be true
-x = FancyPropositions("x")
-y = FancyPropositions("y")
-z = FancyPropositions("z")
 
 
 # Build an example full theory for your setting and return it.
@@ -53,21 +92,29 @@ z = FancyPropositions("z")
 #  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
 #  what the expectations are.
 def example_theory():
+    player_cards = initial_game()
+
     # Add custom constraints by creating formulas with the variables you created. 
-    E.add_constraint((a | b) & ~x)
-    # Implication
-    E.add_constraint(y >> z)
-    # Negate a formula
-    E.add_constraint(~(x & y))
+    print("player cards: ", player_cards)
+    # CCONSTRIANT: If player has card(a,b), then Opponent does not have card(a,b)
+    for card in player_cards:
+        E.add_constraint(Player(card[0], card[1]) >> ~Opponent(card[0], card[1]))
+    
+    #CONSTRAINT: check in player_cards for RUNS
+
+    #CONSTRAINT: check in player_cards for SETS
+
+    #CONSTRAINT: If player does not have the card and opponent have no 
+
+
     # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
     # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
-    constraint.add_exactly_one(E, a, b, c)
+    # constraint.add_exactly_one(E, a, b, c)
 
     return E
 
 
 if __name__ == "__main__":
-
     T = example_theory()
     # Don't compile until you're finished adding all your constraints!
     T = T.compile()
@@ -77,9 +124,9 @@ if __name__ == "__main__":
     print("# Solutions: %d" % count_solutions(T))
     print("   Solution: %s" % T.solve())
 
-    print("\nVariable likelihoods:")
-    for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
-        # Ensure that you only send these functions NNF formulas
-        # Literals are compiled to NNF here
-        print(" %s: %.2f" % (vn, likelihood(T, v)))
+    # print("\nVariable likelihoods:")
+    # for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
+    #     # Ensure that you only send these functions NNF formulas
+    #     # Literals are compiled to NNF here
+    #     print(" %s: %.2f" % (vn, likelihood(T, v)))
     print()
